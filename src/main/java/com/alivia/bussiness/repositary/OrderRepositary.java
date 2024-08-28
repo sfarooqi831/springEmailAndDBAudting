17193fe874788ed80a5ea4eacaaf4ceb5b0c62bb
ifkash <kashaf.farooqi@gmail.com>
2024-08-28 23:59:50 +0500

    File modified for testing

--- src/main/java/com/alivia/bussiness/repositary/OrderRepositary.java
+++ src/main/java/com/alivia/bussiness/repositary/OrderRepositary.java
@@ -5,6 +5,6 @@ import org.springframework.data.repository.history.RevisionRepository;
 
 import com.alivia.bussiness.dto.Order;
 
-public interface OrderRepositary extends RevisionRepository<Order, Integer, Integer>, JpaRepository<Order, Integer> {
+public  interface OrderRepositary extends RevisionRepository<Order, Integer, Integer>,  JpaRepository<Order, Integer> {
 
 }
